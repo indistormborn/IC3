70e470a3332bb303a2a10018814ab543bbf6e302
Indiara C M <indicm@gmail.com>
2019-07-07 18:35:17 -0300

    Arvore Sintatica
    Signed-off-by: Indiara C M <indicm@gmail.com>

---
+++ src/syntacticTree/GeneralNode.java
@@ -0,0 +1,13 @@
+package syntacticTree;
+
+import parser.Token;
+
+abstract public class GeneralNode {
+	public Token position;
+	public int number;
+
+	public GeneralNode (Token x) {
+		position = x;
+		number = 0;
+	}
+}
